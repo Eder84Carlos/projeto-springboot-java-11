b2bf1cae3167676a01323357b5e7d4b86963f025
Eder Carvalho <91353220+Eder84Carlos@users.noreply.github.com>
2021-12-12 21:39:46 -0300

    Project created

---
+++ src/main/java/com/ederweb/eder/EderApplication.java
@@ -0,0 +1,13 @@
+package com.ederweb.eder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ederweb/eder/EderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ederweb.eder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
