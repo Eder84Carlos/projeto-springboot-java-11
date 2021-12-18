d46ee764f6d6787933e9cfa258411423ffca34dd
Eder Carvalho <91353220+Eder84Carlos@users.noreply.github.com>
2021-12-17 23:47:00 -0300

    Teste Postman

---
+++ src/main/java/com/ederweb/eder/services/UserService.java
@@ -0,0 +1,27 @@
+package com.ederweb.eder.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ederweb.eder.entities.User;
+import com.ederweb.eder.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
